35a8782093127b4d99439c8aca732766d6b16230
KRKDEV <111200477+KRKDEV@users.noreply.github.com>
2023-04-11 16:50:45 +0200

    Added Feedback component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,12 @@
 import css from './app.module.css';
 import Section from './Section/Section';
+import FeedbackOptions from './FeedbackOptions/FeedbackOptions';
 export const App = () => {
   return (
     <div className={css.container}>
-      <Section title="Please leave feedback!">Test</Section>
+      <Section title="Please leave feedback!">
+        <FeedbackOptions></FeedbackOptions>
+      </Section>
       <Section title="Statistics">Test</Section>
     </div>
   );
